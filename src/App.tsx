1d0e8f8574c06910f3e5e02e3fa0ef30cf03a4a0
Md. Ridwan Hasan <143118610+RidwanWebWiz@users.noreply.github.com>
2026-09-11 21:16:39 +0600

    TaliwindCSS setup&testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,21 @@ function App() {
 
   return (
     <>
-      
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Hello Tailwind CSS
+        </h1>
 
+        <p className="mt-4 text-gray-600">
+          Tailwind is working in App.tsx!
+        </p>
+
+        <button className="mt-6 px-5 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
+          Click Me
+        </button>
+      </div>
+    </div>
     </>
   )
 }
